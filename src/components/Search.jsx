6a28020f368bca9aebf9991f7a0ad137f103002e
dzksh <dakshdhangar@gmail.com>
2023-05-24 14:28:55 +0530

    3rd commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import { IoMdSearch } from "react-icons/io";
 
 const Search = ({animate, inputValue, handleInput, handleSubmit}) => { 
     return (
-        <form className={`${animate ? 'animate-shake' : 'animate-none'} h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-1 mt-2`}>
+        <form className={`${animate ? 'animate-shake' : 'animate-none'} h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-1 mt-4`}>
         <div className='h-full relative flex items-center justify-between p-2'>
           <input
             value={inputValue}
